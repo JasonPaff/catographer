497773b5eefb165bb683c2f65c4c7efd8b70a838
Jason <jasonpaff@gmail.com>
2024-08-18 01:36:46 -0400

    always keep app awake in dev mode

--- app/settings.tsx
+++ app/settings.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "tamagui";
 export default function SettingsScreen() {
 	return (
 		<View alignItems={"center"} flex={1} justifyContent={"center"}>
-			{process.env.EXPO_PUBLIC_DEV === "true" && <Text fontSize={20}>Settings Screen</Text>}
+			<Text fontSize={20}>Settings Screen</Text>
 		</View>
 	);
 }
